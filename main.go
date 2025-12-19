4115d1fa7c8060485cb8ca3c7879df1c01d90136
Matteo Zulijani <matteo.zulijani@cern.ch>
2025-12-19 11:15:13 +0100

    ahhhhhhhhhhhh

--- main.go
+++ main.go
@@ -1,29 +1,67 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"Github-user-activity/variables"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main(){
-	var argument []string
-	argument = append(argument, os.Args...)
-	fmt.Println(argument)
+	if len(os.Args) != 2 {
+		fmt.Println("usage: github-acitivity <username>")
+		os.Exit(1)
+	}
 	
-	
-	if len(argument) == 2 {
-		fmt.Println(argument[1])
-		get_github_api(argument[1])
-	} else {
-		fmt.Println("program expects one argument")
+	if err := getGithubApi(os.Args[1]); err != nil {
+		log.Fatal(err)
 	}
 	
 }
 
-func get_github_api(username string){
+func getGithubApi(username string) error {
 	variables.Github_url = fmt.Sprintf("https://api.github.com/users/%s/events", username)
 
-	
+	resp, err := http.Get(variables.Github_url)
+	if err != nil {
+		return fmt.Errorf("failed to fetch github api: %w", err)
+	}
+	defer resp.Body.Close()
 
+	var activity []variables.Info
+	if err := json.NewDecoder(resp.Body).Decode(&activity); err != nil {
+		return fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	for _, i := range activity {
+		outputActivity(i)
+	}
+
+	return nil
 }
+
+func outputActivity(activity variables.Info) {
+	switch activity.Type {
+	case "PushEvent":
+		fmt.Printf("- Pushed %d commits to %s.\n", len(activity.Payload.Commits), activity.Repo.Name)
+	case "CreateEvent":
+		fmt.Printf("- Created %s.\n", activity.Repo.Name)
+	case "DeleteEvent":
+		fmt.Printf("- Deleted %s.\n", activity.Repo.Name)
+	case "ForkEvent":
+		fmt.Printf("- Forked %s.\n", activity.Repo.Name)
+	case "IssuesEvent":
+		fmt.Printf("- %s issue #%d on %s.\n", activity.Payload.Action, activity.Payload.Issue.Id, activity.Repo.Name)
+	case "IssueCommentEvent":
+		fmt.Printf("- Commented on issue #%d on %s.\n", activity.Payload.Issue.Id, activity.Repo.Name)
+	case "WatchEvent":
+		fmt.Printf("- Starred %s.\n", activity.Repo.Name)
+	case "PullRequestEvent":
+		fmt.Printf("- %s pull request #%d on %s.\n", activity.Payload.Action, activity.Payload.PULLRequest.Id, activity.Repo.Name)
+	case "PullRequestReviewCommentEvent":
+		fmt.Printf("- Commented on pull request #%d on %s.\n", activity.Payload.PULLRequest.Id, activity.Repo.Name)
+	}
+}
+
--- variables/variables.go
+++ variables/variables.go
@@ -3,5 +3,29 @@ package variables
 var Github_url string
 
 type Info struct {
-	
+	Id    string `json:"id"`
+	Type  string `json:"type"`
+	Actor struct {
+		Id           int    `json:"id"`
+		DisplayLogin string `json:"display_login"`
+	}
+	Repo struct {
+		Id   int    `json:"id"`
+		Name string `json:"name"`
+	}
+	Payload struct {
+		RepoId int    `json:"repository_id"`
+		Action string `json:"action"`
+		Issue  struct {
+			Id int `json:"id"`
+		} `json:"issue"`
+		PULLRequest struct {
+			Id int `json:"id"`
+		} `json:"pull_request"`
+		Commits []struct {
+			Sha     string `json:"sha"`
+			Message string `json:"message"`
+			URL     string `json:"url"`
+		} `json:"commits"`
+	}
 }
